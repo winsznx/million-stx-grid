a48ea4ae0ffc4b7976e9dd1ad578b1dfc105d282
winsznx <timjosh507@gmail.com>
2026-04-14 14:10:57 +0100

    export new coord helpers from client package index

--- packages/stx-canvas-client/src/index.ts
+++ packages/stx-canvas-client/src/index.ts
@@ -1,6 +1,6 @@
 export type { PixelEvent, GridState, CanvasSyncResult } from "./types";
 export { fetchAllPixelEvents } from "./grid/fetchAllEvents";
 export { replayEventsToGrid } from "./grid/replayEvents";
-export { encodeCoord, decodeCoord } from "./grid/encodeCoord";
+export { encodeCoord, decodeCoord, isValidCoord, coordToIndex, indexToCoord } from "./grid/encodeCoord";
 export { useCanvasSync } from "./hooks/useCanvasSync";
 export { getGridDimensions, countFilledPixels, getPixelAt } from "./grid/gridUtils";
