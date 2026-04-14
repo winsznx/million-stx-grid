0d93e8df4b19c2f143597e13122c0bca11b0c882
winsznx <timjosh507@gmail.com>
2026-04-14 15:50:45 +0100

    migrate contract calls to v8 request API with Cl helpers

--- apps/web/lib/contract-calls.ts
+++ apps/web/lib/contract-calls.ts
@@ -1,22 +1,25 @@
-import { openContractCall } from "@stacks/connect";
-import { uintCV, stringAsciiCV } from "@stacks/transactions";
-import { StacksMainnet } from "@stacks/network";
+import { request } from "@stacks/connect";
+import { Cl } from "@stacks/transactions";
 import { CONTRACT_DEPLOYER, CONTRACT_NAME } from "./constants";
 
-export function callPaintPixel(
+export async function callPaintPixel(
   x: number,
   y: number,
   color: string,
   onFinish: () => void,
   onCancel: () => void
-): void {
-  openContractCall({
-    contractAddress: CONTRACT_DEPLOYER,
-    contractName: CONTRACT_NAME,
-    functionName: "paint-pixel",
-    functionArgs: [uintCV(x), uintCV(y), stringAsciiCV(color)],
-    network: new StacksMainnet(),
-    onFinish,
-    onCancel,
-  });
+): Promise<void> {
+  try {
+    const response = await request("stx_callContract", {
+      contract: `${CONTRACT_DEPLOYER}.${CONTRACT_NAME}`,
+      functionName: "paint-pixel",
+      functionArgs: [Cl.uint(x), Cl.uint(y), Cl.stringAscii(color)],
+      network: "mainnet",
+    });
+    if (response.txid) {
+      onFinish();
+    }
+  } catch {
+    onCancel();
+  }
 }
