ec47836567b2678c4d23212cc79e2b0a27ef4bdc
winsznx <timjosh507@gmail.com>
2026-04-14 14:14:34 +0100

    update barrel export for UI components with tooltip

---
+++ apps/web/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { HardShadowButton } from "./HardShadowButton";
+export { LoadingSpinner } from "./LoadingSpinner";
+export { ErrorMessage } from "./ErrorMessage";
+export { Divider } from "./Divider";
+export { Badge } from "./Badge";
+export { ColorSwatch } from "./ColorSwatch";
+export { Tooltip } from "./Tooltip";
