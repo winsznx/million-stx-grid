8359b0ff37a38a279a21e13da6520e2e4a47c6db
winsznx <timjosh507@gmail.com>
2026-04-16 23:37:18 +0100

    feat(sdk): centralize SDK default constants

---
+++ packages/stx-canvas-client/src/constants.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_GRID_SIZE = 100;
+export const DEFAULT_POLL_INTERVAL_MS = 15_000;
+export const DEFAULT_HIRO_API_BASE = "https://api.hiro.so";
+export const HIRO_EVENTS_PAGE_SIZE = 50;
+export const CONTRACT_LOG_EVENT_TYPE = "smart_contract_log";
+export const PIXEL_PAINTED_EVENT_NAME = "pixel-painted";
