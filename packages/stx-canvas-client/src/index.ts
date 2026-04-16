5e21d3a31af3e11ec590c70f16a8b0662dca5f13
winsznx <timjosh507@gmail.com>
2026-04-16 23:37:19 +0100

    chore(sdk): extend public API surface with new helpers

--- packages/stx-canvas-client/src/index.ts
+++ packages/stx-canvas-client/src/index.ts
@@ -6,3 +6,22 @@ export { useCanvasSync } from "./hooks/useCanvasSync";
 export { getGridDimensions, countFilledPixels, getPixelAt } from "./grid/gridUtils";
 export { parseEventRepr } from "./grid/parseEventRepr";
 export { getColorDistribution, getPainterDistribution, getGridFillRate } from "./grid/gridStats";
+export {
+  filterEventsByPainter,
+  filterEventsByColor,
+  filterEventsByRegion,
+  filterEventsByBlockRange,
+} from "./grid/filterEvents";
+export { isValidPixelEvent } from "./grid/validateEvent";
+export { isPixelEvent, assertPixelEvent } from "./grid/guards";
+export { groupBy } from "./grid/groupBy";
+export { getLatestEvent, getEventsAtBlock } from "./grid/latestEvent";
+export { createGridSnapshot, type GridSnapshot } from "./grid/snapshot";
+export {
+  DEFAULT_GRID_SIZE,
+  DEFAULT_POLL_INTERVAL_MS,
+  DEFAULT_HIRO_API_BASE,
+  HIRO_EVENTS_PAGE_SIZE,
+  CONTRACT_LOG_EVENT_TYPE,
+  PIXEL_PAINTED_EVENT_NAME,
+} from "./constants";
