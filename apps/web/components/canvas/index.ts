1b5717b4e75e8d444a1386fcdf7659908d0b453b
winsznx <timjosh507@gmail.com>
2026-04-16 23:37:18 +0100

    chore(canvas): add barrel export for canvas components

---
+++ apps/web/components/canvas/index.ts
@@ -0,0 +1,5 @@
+export { ColorPalette } from "./ColorPalette";
+export { CoordinateDisplay } from "./CoordinateDisplay";
+export { PendingPixelOverlay } from "./PendingPixelOverlay";
+export { PixelCanvas } from "./PixelCanvas";
+export { ZoomControls } from "./ZoomControls";
