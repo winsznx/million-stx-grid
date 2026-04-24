34f09c26798ed7d865b8eb18492de651213ad608
winsznx <timjosh507@gmail.com>
2026-04-24 17:55:08 +0100

    feat: add canvas synchronization hook

--- apps/web/hooks/useCanvasSync.ts
+++ apps/web/hooks/useCanvasSync.ts
@@ -1,1 +1,19 @@
-export { useCanvasSync } from "@winsznx/stx-canvas-client";
+"use client";
+
+import { useEffect, useRef } from "react";
+
+/**
+ * Hook for syncing canvas state with external updates.
+ */
+export function useCanvasSync(onUpdate: () => void, interval = 15000) {
+  const timerRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    timerRef.current = setInterval(onUpdate, interval);
+    return () => {
+      if (timerRef.current) clearInterval(timerRef.current);
+    };
+  }, [onUpdate, interval]);
+
+  return { isSyncing: true };
+}
