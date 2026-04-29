939ea8f89638f58f77edd3c9a5f431a79bcbb9f8
winsznx <timjosh507@gmail.com>
2026-04-29 23:46:13 +0000

    refactor: add typed metadata export to favicon generator

--- apps/web/app/icon.tsx
+++ apps/web/app/icon.tsx
@@ -1,23 +1,14 @@
 import { ImageResponse } from "next/og";
+import { FAVICON_SIZE, DESIGN } from "@/lib/constants";
 
-export const size = { width: 32, height: 32 };
+export const size = { width: FAVICON_SIZE, height: FAVICON_SIZE };
 export const contentType = "image/png";
 
 export default function Icon() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexWrap: "wrap",
-        }}
-      >
-        <div style={{ width: 16, height: 16, background: "#00ff94" }} />
-        <div style={{ width: 16, height: 16, background: "#7b61ff" }} />
-        <div style={{ width: 16, height: 16, background: "#ff3c6e" }} />
-        <div style={{ width: 16, height: 16, background: "#0a0a0f" }} />
+      <div style={{ width: FAVICON_SIZE, height: FAVICON_SIZE, display: "flex", alignItems: "center", justifyContent: "center", background: DESIGN.bg, borderRadius: 4 }}>
+        <div style={{ width: 20, height: 20, background: DESIGN.primaryNeon, borderRadius: 2 }} />
       </div>
     ),
     { ...size }
