880df22fd3bb5c5ae105a1326c5460bb5392b530
winsznx <timjosh507@gmail.com>
2026-04-30 01:30:13 +0000

    refactor: organize barrel exports with explicit re-exports

--- packages/stx-canvas-client/src/index.ts
+++ packages/stx-canvas-client/src/index.ts
@@ -1,27 +1,22 @@
-export type { PixelEvent, GridState, CanvasSyncResult } from "./types";
-export { fetchAllPixelEvents } from "./grid/fetchAllEvents";
-export { replayEventsToGrid } from "./grid/replayEvents";
-export { encodeCoord, decodeCoord, isValidCoord, coordToIndex, indexToCoord } from "./grid/encodeCoord";
-export { useCanvasSync } from "./hooks/useCanvasSync";
-export { getGridDimensions, countFilledPixels, getPixelAt } from "./grid/gridUtils";
-export { parseEventRepr } from "./grid/parseEventRepr";
-export { getColorDistribution, getPainterDistribution, getGridFillRate } from "./grid/gridStats";
-export {
-  filterEventsByPainter,
-  filterEventsByColor,
-  filterEventsByRegion,
-  filterEventsByBlockRange,
-} from "./grid/filterEvents";
-export { isValidPixelEvent } from "./grid/validateEvent";
+// Core types
+export type { PixelData, PixelEvent, GridState, FrequencyEntry } from "./types";
+export { HIRO_EVENTS_PAGE_SIZE, CLIENT_GRID_SIZE, TOTAL_GRID_CELLS } from "./constants";
+
+// Grid utilities
+export { encodeCoord, decodeCoord, isValidCoord } from "./grid/encodeCoord";
+export { getPixel, isPixelPainted, getPaintedCoordinates, getPaintedCount } from "./grid/gridUtils";
 export { isPixelEvent, assertPixelEvent } from "./grid/guards";
 export { groupBy } from "./grid/groupBy";
 export { getLatestEvent, getEventsAtBlock } from "./grid/latestEvent";
-export { createGridSnapshot, type GridSnapshot } from "./grid/snapshot";
-export {
-  DEFAULT_GRID_SIZE,
-  DEFAULT_POLL_INTERVAL_MS,
-  DEFAULT_HIRO_API_BASE,
-  HIRO_EVENTS_PAGE_SIZE,
-  CONTRACT_LOG_EVENT_TYPE,
-  PIXEL_PAINTED_EVENT_NAME,
-} from "./constants";
+export { filterEventsByPainter, filterEventsByColor, filterEventsByRegion, filterEventsByBlockRange } from "./grid/filterEvents";
+export { isValidPixelEvent } from "./grid/validateEvent";
+export { getColorDistribution, getPainterDistribution, getGridFillRate } from "./grid/gridStats";
+export { replayEventsToGrid } from "./grid/replayEvents";
+export { parseEventRepr } from "./grid/parseEventRepr";
+export type { ParsedPixelEvent } from "./grid/parseEventRepr";
+export { createGridSnapshot } from "./grid/snapshot";
+export type { GridSnapshot } from "./grid/snapshot";
+export { fetchAllPixelEvents } from "./grid/fetchAllEvents";
+
+// React hook
+export { useCanvasSync } from "./hooks/useCanvasSync";
